0d1ffb2f97b1e097c8304ecb7e4562fb203bb7f5
suzhe <2212294193@qq.com>
2025-03-11 17:36:45 +0800

    fix: AppCompatActivity的主题需要使用AppCompat的主题

--- app/src/main/java/com/suzhe/playdemo/MainActivity.kt
+++ app/src/main/java/com/suzhe/playdemo/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.suzhe.playdemo
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+    }
 }
 
