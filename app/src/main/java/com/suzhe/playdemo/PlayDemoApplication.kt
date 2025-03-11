ce3a5909439f58bec6eb73d1371321c418772479
suzhe <2212294193@qq.com>
2025-03-11 17:51:16 +0800

    增加PlayDemoApplication
    - 使用 Timber 输出log日志
    - 使用 XCrash 监测程序崩溃
    - 将崩溃日志复制到指定位置

---
+++ app/src/main/java/com/suzhe/playdemo/PlayDemoApplication.kt
@@ -0,0 +1,90 @@
+package com.suzhe.playdemo
+
+import android.app.Application
+import android.content.ContentValues
+import android.content.Context
+import android.provider.MediaStore
+import com.tencent.mmkv.MMKV
+import kotlinx.coroutines.*
+import timber.log.Timber
+import xcrash.TombstoneManager
+import xcrash.XCrash
+
+/**
+ * 全局Application
+ */
+class PlayDemoApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.IO)
+
+    //region 生命周期方法
+    /**
+     * 附加应用程序的上下文。
+     * 用于初始化 XCrash 崩溃监控。
+     * @param base 应用的基础 Context
+     */
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        XCrash.init(this)// 初始化 XCrash 进行崩溃监控
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+        initMMKV()
+        processCrashLogs()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        applicationScope.cancel() // 应用终止时取消协程作用域
+    }
+    //endregion
+
+    private fun initTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
+
+    //region 处理应用崩溃日志
+    private fun processCrashLogs() {
+        applicationScope.launch(Dispatchers.IO) {
+            delay(5000) // 延迟 5 秒执行
+            for (file in TombstoneManager.getAllTombstones()) {
+                try {
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.MediaColumns.DISPLAY_NAME, file.name)
+                        put(MediaStore.MediaColumns.MIME_TYPE, "txt")
+                    }
+                    val uri = contentResolver.insert(MediaStore.Files.getContentUri("external"), contentValues) ?: continue
+                    contentResolver.openOutputStream(uri)?.use { out ->
+                        file.inputStream().use { ins ->
+                            ins.copyTo(out) // 复制崩溃日志文件内容
+                            Timber.i("Copy file to $out")
+                        }
+                    }
+                    file.delete() // 删除原始日志文件
+                } catch (ignore: Throwable) {
+                    // 忽略异常，防止崩溃
+                }
+            }
+        }
+    }
+    //endregion
+
+    /**
+     * 初始化 腾讯开源的高性能keyValue存储，用来替代系统的SharedPreferences
+     */
+    private fun initMMKV() {
+        val rootDir = MMKV.initialize(this)
+        Timber.d("initMMKV %s", rootDir)
+    }
+
+    companion object {
+        /**
+         * 这里是为了以后能方便的调用 PlayDemoApplication 里面的一些方法，如 initOCR() 等
+         */
+        lateinit var instance: PlayDemoApplication
+    }
+
+}
