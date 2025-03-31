08a7d1136b417ad71e6e19d60eb08a2a23f0a43a
suzhe <2212294193@qq.com>
2025-03-31 11:20:17 +0800

    feat: SplashActivity的创建-权限请求的实现

---
+++ app/src/main/java/com/suzhe/playdemo/utils/PreferenceUtil.kt
@@ -0,0 +1,45 @@
+package com.suzhe.playdemo.utils
+
+import com.tencent.mmkv.MMKV
+import java.lang.reflect.Array.setBoolean
+
+/**
+ * 偏好设置工具类
+ */
+object PreferenceUtil {
+    val p: MMKV by lazy {
+        MMKV.defaultMMKV()!!
+    }
+
+    private const val ACCEPT_TERM = "Accept_Term"
+
+    fun isAcceptTermsServiceAgreement(): Boolean {
+        return getBoolean(ACCEPT_TERM,false)
+    }
+
+    fun setAcceptTermsServiceAgreement() {
+        return putBoolean(ACCEPT_TERM,true)
+    }
+
+    //region 辅助方法
+    private fun getString(key: String): String? {
+        return p.decodeString(key, null)
+    }
+
+    private fun putString(key: String, value: String) {
+        p.edit().putString(key, value).apply()
+    }
+
+    private fun delete(data: String) {
+        p.edit().remove(data).commit()
+    }
+
+    private fun getBoolean(key: String, defaultValue: Boolean): Boolean {
+        return p.getBoolean(key, defaultValue)
+    }
+
+    private fun putBoolean(key: String, value: Boolean) {
+        p.edit().putBoolean(key, value).apply()
+    }
+    //endregion
+}
