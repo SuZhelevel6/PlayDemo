da0491c315958f8b4ebec406b22e6aef1fba0876
suzhe <2212294193@qq.com>
2025-03-31 13:54:24 +0800

    feat: SplashActivity的创建-用户隐私协议弹窗的实现

---
+++ app/src/main/java/com/suzhe/playdemo/base/fragment/BaseCommonFragment.kt
@@ -0,0 +1,56 @@
+package com.suzhe.playdemo.base.fragment
+
+import android.content.Intent
+import android.view.View
+import androidx.annotation.IdRes
+import com.suzhe.playdemo.utils.Constants
+
+
+abstract class BaseCommonFragment : BaseFragment() {
+    fun <T : View?> findViewById(@IdRes id: Int): T {
+        return requireView().findViewById(id)
+    }
+
+    /**
+     * 获取int值
+     *
+     * @param key
+     * @return
+     */
+    protected fun extraInt(key: String?): Int {
+        return requireArguments().getInt(key, -1)
+    }
+
+    /**
+     * 启动界面，可以传递一个字符串参数
+     *
+     * @param clazz
+     * @param id
+     */
+    protected fun startActivityExtraId(
+        clazz: Class<*>,
+        id: String
+    ) {
+        //创建Intent
+        val intent = Intent(activity, clazz).apply {
+            //传递数据
+            //不为空才传递
+            putExtra(Constants.ID, id)
+        }
+
+        //启动界面
+        startActivity(intent)
+    }
+
+    /**
+     * 启动界面
+     * @param clazz
+     */
+    protected fun startActivity(clazz: Class<*>) {
+        //创建Intent
+        val intent = Intent(requireActivity(), clazz)
+
+        //启动界面
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/suzhe/playdemo/component/splash/TermServiceDialogFragment.kt
+++ app/src/main/java/com/suzhe/playdemo/component/splash/TermServiceDialogFragment.kt
@@ -1,4 +1,76 @@
 package com.suzhe.playdemo.component.splash
 
-class TermServiceDialogFragment {
+import android.text.Html
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.content.ContextCompat.getColor
+import androidx.fragment.app.FragmentManager
+import com.blankj.utilcode.util.AppUtils
+import com.blankj.utilcode.util.ScreenUtils
+import com.suzhe.playdemo.R
+import com.suzhe.playdemo.base.fragment.BaseViewModelDialogFragment
+import com.suzhe.playdemo.databinding.FragmentDialogTermServiceBinding
+
+class TermServiceDialogFragment : BaseViewModelDialogFragment<FragmentDialogTermServiceBinding>() {
+    private lateinit var onAgreementClickListener: View.OnClickListener
+
+    override fun initViews() {
+        super.initViews()
+        //点击弹窗外边不能关闭
+        isCancelable = false
+
+        binding.content.apply {
+            // 设置后才可以点击
+            movementMethod = LinkMovementMethod.getInstance()
+            // 链接的颜色
+            setLinkTextColor(getColor(requireContext(), R.color.link))
+        }
+
+    }
+
+    override fun initDatum() {
+        super.initDatum()
+        val content = Html.fromHtml(getString(R.string.term_service_privacy_content))
+        binding.content.text = content
+    }
+
+    override fun initListeners() {
+        super.initListeners()
+        //同意按钮点击
+        binding.primary.setOnClickListener {
+            dismiss()
+            onAgreementClickListener.onClick(it)
+        }
+
+        //不同意按钮点击
+        binding.disagree.setOnClickListener {
+            dismiss()
+            AppUtils.exitApp()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //修改宽度，默认比AlertDialog.Builder显示对话框宽度窄，看着不好看
+        //参考：https://stackoverflow.com/questions/12478520/how-to-set-dialogfragments-width-and-height
+        val params: ViewGroup.LayoutParams = dialog!!.window!!.attributes
+        params.width = ((ScreenUtils.getScreenWidth() * 0.9).toInt())
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog!!.window!!.attributes = params as WindowManager.LayoutParams
+    }
+
+    companion object {
+        /**
+         * 显示对话框
+         */
+        fun show(fragmentManager: FragmentManager, onAgreementClickListener: View.OnClickListener) {
+            val dialogFragment = TermServiceDialogFragment()
+
+            dialogFragment.onAgreementClickListener = onAgreementClickListener
+
+            dialogFragment.show(fragmentManager, "TermServiceDialogFragment")
+        }
+    }
 }
